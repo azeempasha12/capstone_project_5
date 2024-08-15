1a47cb06bf299fa6946dd12343485d3597dbb70e
Azeem pasha <147592984+azeempasha12@users.noreply.github.com>
2024-08-15 13:41:42 +0530

    Update vite.config.js

--- frontend/AB-Movies/vite.config.js
+++ frontend/AB-Movies/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-})
+  plugins: [react()],
+  build: {
+    outDir: 'build', // Ensure this matches your publish directory
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+});
